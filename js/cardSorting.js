6cabb66913e83ce66321f7c2545dff747a21ac3e
Lindsey Thompson <lindseyatx97@gmail.com>
2018-10-21 15:42:19 +1100

    Filter js completed

--- js/cardSorting.js
+++ js/cardSorting.js
@@ -1,19 +1,41 @@
 //****** Card Sorting ******//
-
-filterSelection("all") // Execute the function and show all columns
+showAllCards();
 
 function filterSelection(filterName) {
-  var cards = document.getElementsByClassName("column");
-  if (filterName == "all") {
-    filterName = "";
+  var filterCards = document.getElementsByClassName(filterName);
+  var isShowing = false;
+
+  // if one of the elements has a show than it is a show
+  if (filterCards[0].className.indexOf("show") > -1) {
+    isShowing = true;
   }
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (var i = 0; i < cards.length; i++) {
-    removeClass(cards[i], "show");
-    if (cards[i].className.indexOf(filterName) > -1) {
-      addClass(cards[i], "show");
+
+  if (isShowing) {
+    //remove all show, button gray
+    for (var i = 0; i < filterCards.length; i++) {
+      removeClass(filterCards[i], "show");
+    }
+  } else {
+    //add all show
+    for (var i = 0; i < filterCards.length; i++) {
+      addClass(filterCards[i], "show");
     }
   }
+  changeButtonColor(filterName);
+}
+
+// Show all categories
+function showAllCards() {
+  var cards = document.getElementsByClassName("column");
+  for (var i = 0; i < cards.length; i++) {
+     addClass(cards[i], "show");
+  }
+  setActiveButton("food");
+  setActiveButton("travel");
+  setActiveButton("home");
+  setActiveButton("care");
+  setActiveButton("stuff");
+  setActiveButton("misc");
 }
 
 // Show filtered elements
@@ -33,13 +55,36 @@ function removeClass(element) {
   element.className = elementClasses.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("button-container");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+function changeButtonColor(filterName) {
+  var buttonId = filterName + "-btn";
+  var currentElement = document.getElementById(buttonId);
+
+  if (currentElement.className.indexOf("active") == -1) {
+    currentElement.className += " active";
+  } else {
+    currentElement.className = currentElement.className.replace(" active", "");
+  }
+  console.log(currentElement.className);
+
 }
+
+function setActiveButton(filterName) {
+  var buttonId = filterName + "-btn";
+  var currentElement = document.getElementById(buttonId);
+  if (currentElement.className.indexOf("active") == -1) {
+    currentElement.className += " active";
+  }
+  console.log(currentElement.className);
+
+}
+
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("button-container");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
